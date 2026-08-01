86448e870744c4378f774d9f59d3ec4760c43a26
Arun Krishna <arunkrishnaraghav.com>
2026-08-01 11:51:28 +0530

    backend url change after deployment

--- music-catalog-frontend/src/services/api.ts
+++ music-catalog-frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth } from "firebase/auth";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(async (config) => {
