bb665482452fe446d127457334836465aaf48f54
Arun Krishna <arunkrishnaraghav.com>
2026-08-01 11:12:18 +0530

    "Configure backend for Render deployment"

--- music-catalog-backend/src/main/java/com/arun/musiccatalog/config/FirebaseConfig.java
+++ music-catalog-backend/src/main/java/com/arun/musiccatalog/config/FirebaseConfig.java
@@ -1,27 +1,29 @@
 package com.arun.musiccatalog.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import com.google.cloud.firestore.Firestore;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
-    public FirebaseApp firebaseApp() throws IOException {
+    public FirebaseApp firebaseApp() throws Exception {
 
-        GoogleCredentials credentials =
-                GoogleCredentials.fromStream(
-                        new ClassPathResource("firebase/firebase-service-account.json").getInputStream()
-                );
+        String firebaseJson = System.getenv("FIREBASE_SERVICE_ACCOUNT");
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                new ByteArrayInputStream(
+                        firebaseJson.getBytes(StandardCharsets.UTF_8)
+                )
+        );
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(credentials)
